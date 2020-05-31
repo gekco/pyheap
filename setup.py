6b39d52d77b2341945ab20cec9a966a2a2182339
Ankit Kathuria <ankitkathuria@Ankits-MacBook-Air.local>
2020-05-31 15:43:32 +0530

    Set up py change

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='heaps',
     packages=['heapy'],
-    version='0.0.2',
+    version='0.0.3',
     license='MIT',
     description='Better Heaps for python',
     author='Ankit Kathuria',
